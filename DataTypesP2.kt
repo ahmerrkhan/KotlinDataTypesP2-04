cd4d0345c1ac29980e6e45734c4fa023a460f665
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2020-10-09 00:00:17 +0500

    datatypes part 2
    basics

---
+++ DataTypesP2.kt
@@ -0,0 +1,23 @@
+
+//Data types Part 2
+fun main(args : Array<String>){
+
+    //1. Characters
+    val _alphabet1 : Char
+    _alphabet1 = 'X'
+    var _alphabet2 : Char
+    _alphabet2 = 'Y'
+
+    println("First character is : "+_alphabet1)
+    println("Second character is : "+_alphabet2)
+
+    //2. Boolean
+    val flag1 : Boolean = true
+    val flag2 : Boolean = false
+
+    println("Value 1 is : "+flag1)
+    println("Value 2 is : "+flag2)
+
+
+
+}
